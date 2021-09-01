a365e6aadff370fd70251f19c26a663ff809e1f3
ArisGoi <aris.goi97@gmail.com>
2021-09-01 16:17:45 +0200

    add childGenerator

--- main.js
+++ main.js
@@ -2,3 +2,13 @@
 // Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
 // Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
 
+// Genera un numero definito di figli all'interno di un'elemento
+function childGenerator(nameContainElement, typeOfChild, nameChildClass, numberOfChild){
+    for (i = 0; i < numberOfChild; i++) {
+        document.getElementById(nameContainElement).innerHTML += `<${typeOfChild} class="${nameChildClass}">${i + 1}</${typeOfChild}>`
+    }
+}
+
+childGenerator("container", "div", "square", 50);
+
+// document.getElementById(nameElement).innerHTML = `<div class="${nameSquareClass}">${i}</div>`
